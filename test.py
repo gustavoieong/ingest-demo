59a1b5dd17b15a0f0a136e17b14dd0e1ff5b4560
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 12:55:57 -0500

    release

---
+++ test.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import time
+
+mysql_user = "admin"
+mysql_password = "Azure123."
+mysql_host = "database-api.conxcscqngr8.us-east-1.rds.amazonaws.com"
+mysql_database = "database_rest_api"
+
+# Connect to MySQL database
+cnx = mysql.connector.connect(user = mysql_user, password = mysql_password,
+                              host = mysql_host, database = mysql_database)
+
+# Create a cursor object
+#cursor = cnx.cursor()
+cursor = cnx.cursor(buffered=True)
+
+# Execute a SELECT query
+query = """SELECT COUNT(*) cant FROM tb_jobs;"""
+cursor.execute(query)
+
+# Fetch all rows and print them
+for row in cursor.fetchall():
+    print("cantidad: ")
+    print(row)
+
+# Close the cursor and database connections
+cursor.close()
+cnx.close()
