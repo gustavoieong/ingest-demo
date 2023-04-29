3aa40f755f1056eb581d3998f9bfd1c720a431fe
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 00:59:56 -0500

    release

---
+++ view.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+mysql_user = "admin"
+mysql_password = "Azure123."
+mysql_host = "database-api.conxcscqngr8.us-east-1.rds.amazonaws.com"
+mysql_database = "database_rest_api"
+
+# Connect to MySQL database
+cnx = mysql.connector.connect(user = mysql_user, password = mysql_password,
+                              host = mysql_host, database = mysql_database)
+
+# Create a cursor object
+cursor = cnx.cursor()
+
+# Execute a SELECT query
+query = "SELECT * FROM tb_departments"
+cursor.execute(query)
+
+# Fetch all rows and print them
+for row in cursor.fetchall():
+    print(row)
+
+# Execute a SELECT query
+query = "SELECT * FROM tb_hired_employees"
+cursor.execute(query)
+
+# Fetch all rows and print them
+for row in cursor.fetchall():
+    print(row)
+
+# Execute a SELECT query
+query = "SELECT * FROM tb_jobs"
+cursor.execute(query)
+
+# Fetch all rows and print them
+for row in cursor.fetchall():
+    print(row)
+
+# Close the cursor and database connections
+cursor.close()
+cnx.close()
