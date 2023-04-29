c1fa5ee279bc6ad8fc862b976b475658bd3a2357
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 15:39:33 -0500

    release

--- sender.py
+++ sender.py
@@ -4,13 +4,13 @@ import sys
 url = "http://127.0.0.1:8000/uploadfile"
 
 try:
-    #with open("input/departments.csv", "rb") as f:
-    #    r = requests.post(url, files={"file": f})
-    #    print(r.json())
+    with open("input/departments.csv", "rb") as f:
+        r = requests.post(url, files={"file": f})
+        print(r.json())
 
-    #with open("input/hired_employees.csv", "rb") as f:
-    #    r = requests.post(url, files={"file": f})
-    #    print(r.json())
+    with open("input/hired_employees.csv", "rb") as f:
+        r = requests.post(url, files={"file": f})
+        print(r.json())
 
     with open("input/jobs.csv", "rb") as f:
         r = requests.post(url, files={"file": f})
