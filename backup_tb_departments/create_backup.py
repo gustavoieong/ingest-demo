4c8350119445e1fffab68876f1e49a14b4a78c62
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 18:41:58 -0500

    release

--- backup_tb_departments/create_backup.py
+++ backup_tb_departments/create_backup.py
@@ -13,31 +13,35 @@ schema = {
     ]
 }
 
-# Connect to MySQL database
-cnx = mysql.connector.connect(user='admin', password='Azure123.',
-                              host='database-api.conxcscqngr8.us-east-1.rds.amazonaws.com', database='database_rest_api')
-cursor = cnx.cursor()
+def main():
+    # Connect to MySQL database
+    cnx = mysql.connector.connect(user='admin', password='Azure123.',
+                                host='database-api.conxcscqngr8.us-east-1.rds.amazonaws.com', database='database_rest_api')
+    cursor = cnx.cursor()
 
-# Execute SELECT query to retrieve data from table
-query = ("SELECT id, department FROM tb_departments")
-cursor.execute(query)
+    # Execute SELECT query to retrieve data from table
+    query = ("SELECT id, department FROM tb_departments")
+    cursor.execute(query)
 
-# Open file for writing Avro data
-filename = 'backup_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.avro'
-with open(filename, 'wb') as avro_file:
+    # Open file for writing Avro data
+    filename = 'backup_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.avro'
+    with open(filename, 'wb') as avro_file:
 
-    # Initialize Avro writer
-    avro_writer = fastavro.writer(avro_file, schema, codec='deflate')
+        # Initialize Avro writer
+        avro_writer = fastavro.writer(avro_file, schema, codec='deflate')
 
-    # Write data to Avro file in batches
-    batch_size = 100
-    while True:
-        rows = cursor.fetchmany(batch_size)
-        if not rows:
-            break
-        for row in rows:
-            avro_writer.write({'id': row[0], 'department': row[1]})
+        # Write data to Avro file in batches
+        batch_size = 100
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            for row in rows:
+                avro_writer.write({'id': row[0], 'department': row[1]})
 
-# Close cursor and database connection
-cursor.close()
-cnx.close()
+    # Close cursor and database connection
+    cursor.close()
+    cnx.close()
+
+if __name__ == "__main__":
+    main()
