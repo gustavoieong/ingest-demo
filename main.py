b07c1abdf59995c6d2963685a1e1134ed300e9a7
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 14:59:58 -0500

    release

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
 import csv
+import io
 import mysql.connector
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    # connect to MySQL database
-    cnx = mysql.connector.connect(
-        host='database-api.conxcscqngr8.us-east-1.rds.amazonaws.com',
-        user='admin',
-        password='Azure123.',
-        database='database_rest_api'
-    )
+# Define MySQL connection parameters
+config = {
+    'user': 'admin',
+    'password': 'Azure123.',
+    'host': 'database-api.conxcscqngr8.us-east-1.rds.amazonaws.com',
+    'database': 'database_rest_api',
+    'raise_on_warnings': True
+}
+
+# Define batch size
+batch_size = 100
+
+@app.post("/upload-csv/")
+async def upload_csv(file: UploadFile = File(...)):
+
+    # Connect to MySQL
+    cnx = mysql.connector.connect(**config)
+
+    # Create cursor
     cursor = cnx.cursor()
 
-    # read CSV file
-    #csv_reader = csv.DictReader(await file.read().decode('utf-8').splitlines())
-    archivo = file.read()
-    #archivo = archivo.decode('utf-8')
-    #archivo = archivo.splitlines()
-    csv_reader = csv.DictReader(await archivo)
-    #csv_reader = csv.DictReader(await file.read().decode('utf-8').splitlines())
+    # Read CSV file
+    csv_file = io.StringIO(await file.read().decode())
+    csv_reader = csv.reader(csv_file)
 
-    # define SQL statement
-    insert_stmt = "INSERT INTO tb_jobs (id, job) VALUES (%d, %s)"
-    
-    # insert data in batches of 100 rows
-    batch_size = 100
+    # Skip header row
+    next(csv_reader)
+
+    # Initialize batch
     batch = []
+
+    # Iterate over rows
     for row in csv_reader:
-        batch.append((row['id'], row['job']))
-        if len(batch) >= batch_size:
-            cursor.executemany(insert_stmt, batch)
+        # Extract data
+        id = row[0]
+        job = row[1]
+
+        # Append data to batch
+        batch.append((id, job))
+
+        # If batch size reached, insert data into MySQL
+        if len(batch) == batch_size:
+            cursor.executemany("INSERT INTO youtb_jobsr_table (id, job) VALUES (%d, %s)", batch)
             batch = []
-    
-    # insert any remaining rows
+
+    # Insert any remaining rows
     if len(batch) > 0:
-        cursor.executemany(insert_stmt, batch)
-        
-    # commit changes to database and close connection
+        cursor.executemany("INSERT INTO tb_jobs (id, job) VALUES (%d, %s)", batch)
+
+    # Commit changes and close connection
     cnx.commit()
     cursor.close()
     cnx.close()
 
-    return {"filename": file.filename}
+    return {"message": "CSV file uploaded and inserted into MySQL database."}
