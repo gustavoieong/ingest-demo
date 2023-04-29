d540e5963bcf9a4da77cfcf564a184de3bfd5f92
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 00:28:24 -0500

    release

--- sender.py
+++ sender.py
@@ -1,15 +1,21 @@
 import requests
+import sys
 
 url = "http://127.0.0.1:8000/uploadfile"
 
-with open("input/departments.csv", "rb") as f:
-    r = requests.post(url, files={"file": f})
-    #print(r.json())
+try:
+    with open("input/departments.csv", "rb") as f:
+        r = requests.post(url, files={"file": f})
+        #print(r.json())
 
-with open("input/hired_employees.csv", "rb") as f:
-    r = requests.post(url, files={"file": f})
-    #print(r.json())
+    with open("input/hired_employees.csv", "rb") as f:
+        r = requests.post(url, files={"file": f})
+        #print(r.json())
 
-with open("input/jobs.csv", "rb") as f:
-    r = requests.post(url, files={"file": f})
-    #print(r.json())
+    with open("input/jobs.csv", "rb") as f:
+        r = requests.post(url, files={"file": f})
+        #print(r.json())
+
+except Exception as e:
+    print("Error: ", e)
+    sys.exit("Fin de la ejecucion con error.")
