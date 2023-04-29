259e09bb20154940516f8ed55f22ccaadf3fa361
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 11:17:45 -0500

    release

--- main.py
+++ main.py
@@ -1,111 +1,53 @@
-import csv
+from flask import Flask, request
 import mysql.connector
-from fastapi import FastAPI, File, UploadFile
-import traceback
 
-app = FastAPI()
+app = Flask(__name__)
 
-def generate_table(filename):
-    create_table_query = ""
-    if (filename == "departments.csv"):
-        create_table_query = """CREATE TABLE IF NOT EXISTS tb_departments (
-                             id INT,
-                             name VARCHAR(255),
-                             datetime VARCHAR(255),
-                             department_id INT,
-                             job_id INT
-                             );"""
-    if (filename == "hired_employees.csv"):
-        create_table_query = """CREATE TABLE IF NOT EXISTS tb_hired_employees (
-                             id INT,
-                             department VARCHAR(255)
-                             );"""
-    if (filename == "jobs.csv"):
-        create_table_query = """CREATE TABLE IF NOT EXISTS tb_jobs (
-                             id INT,
-                             job VARCHAR(255)
-                             );"""
-    return create_table_query
+# MySQL configuration
+db_config = {
+    'host': 'database-api.conxcscqngr8.us-east-1.rds.amazonaws.com',
+    'user': 'admin',
+    'password': 'Azure123.',
+    'database': 'database_rest_api',
+}
 
-def generate_query(filename):
-    insert_query = ""
-    if (filename == "departments.csv"):
-        insert_query = """INSERT INTO my_table (id, name, datetime, department_id, job_id) VALUES (%d, %s, %s, %d, %d);"""
-    if (filename == "departments.csv"):
-        insert_query = """INSERT INTO my_table (id, job) VALUES (%d, %s);"""
-    if (filename == "departments.csv"):
-        insert_query = """INSERT INTO my_table (id, job) VALUES (%d, %s);"""
-    return insert_query
+# Endpoint to receive CSV file
+@app.route('/upload_csv', methods=['POST'])
+def upload_csv():
+    # Connect to MySQL
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    # Parse the CSV file
-    contents = await file.read()
-    reader = csv.reader(contents.decode().splitlines())
-    #print(contents.decode())
-    print(file.filename)
+    # Get CSV file from request
+    file = request.files['file']
 
-    mysql_user = "admin"
-    mysql_password = "Azure123."
-    mysql_host = "database-api.conxcscqngr8.us-east-1.rds.amazonaws.com"
-    mysql_database = "database_rest_api"
-
-    # Connect to the MySQL database
-    cnx = mysql.connector.connect(user = mysql_user, password = mysql_password, host = mysql_host, database = mysql_database)
-    cursor = cnx.cursor()
-
-    # Create table if it doesn't exist
-    create_table_query = generate_table(file.filename)
-    cursor.execute(create_table_query)
-
-    # Set insert query
-    insert_query = generate_query(file.filename)
-
-    # Open the CSV file for reading
-  
-
-    # Insert rows in batches of 1000
-    batch_size = 1000
-    batch = []
-    for row in reader:
-        # Append the row to the batch
-        batch.append(row)
-
-        # If the batch is full, insert the rows and reset the batch
-        if len(batch) == batch_size:
-            insert_query = generate_query(file.filename)
-            try:
-                cursor.executemany(insert_query)
-            except Exception as e:
-                # log the error message to a file
-                damage_row = str(row)
-                with open('error_log.txt', 'a') as f:
-                    f.write(f"Error message: {str(e)}\n")
-                    f.write(f"Error message: {str(damage_row)}\n")
-                    f.write(traceback.format_exc() + "\n")
-                # continue execution, skipping the line that raised the exception
-                pass
-            cnx.commit()
-            batch = []
+    # Loop through the file and insert data into MySQL in batches of 1000 rows
+    rows = []
+    count = 0
+    for line in file:
+        # Convert bytes to string and split by comma
+        row = line.decode().strip().split(',')
+        # Append row to rows list
+        rows.append(tuple(row))
+        count += 1
+        # Insert rows into MySQL in batches of 1000
+        if count % 1000 == 0:
+            insert_query = "INSERT INTO tb_jobs (id, job) VALUES (%d, %s);"
+            cursor.executemany(insert_query, rows)
+            conn.commit()
+            rows = []
 
     # Insert any remaining rows
-    if batch:
-        insert_query = generate_query(file.filename)
-        try:
-            cursor.executemany(insert_query)
-        except Exception as e:
-            # log the error message to a file
-            damage_row = str(row)
-            with open('error_log.txt', 'a') as f:
-                f.write(f"Error message: {str(e)}\n")
-                f.write(f"Error message: {str(damage_row)}\n")
-                f.write(traceback.format_exc() + "\n")
-            # continue execution, skipping the line that raised the exception
-            pass
-        cnx.commit()
+    if rows:
+        insert_query = "INSERT INTO tb_jobs VALUES (%d, %s)"
+        cursor.executemany(insert_query, rows)
+        conn.commit()
 
-    # Close the cursor and connection
+    # Close MySQL connection
     cursor.close()
-    cnx.close()
+    conn.close()
 
-    return {"filename": file.filename}
+    return 'CSV file uploaded and inserted into MySQL.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
