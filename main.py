3e1b06e129e1efb06d3af3b74330c1fd96a47dc1
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-29 15:38:11 -0500

    release

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ mysql_config = {
     'database': 'database_rest_api'
 }
 
-# Define the INSERT query to insert rows into the MySQL table
-insert_query = "INSERT INTO tb_jobs (id, job) VALUES (%s, %s)"
+
 
 # Define the batch size
 batch_size = 100
@@ -26,15 +25,37 @@ async def create_upload_file(file: UploadFile = File(...)):
     # Read the CSV file in batches of 100 rows
     rows = []
     for line_no, line in enumerate(file.file):
-        # Skip the header line
-        if line_no == 0:
-            continue
         # Parse the line to get the values
         values = line.decode('utf-8').strip().split(',')
-        id = int(values[0])
-        job = values[1]
-        # Append the row to the rows list
-        rows.append((id, job))
+
+        if(file.filename == 'departments.csv'):
+            id = int(values[0])
+            department = values[1]
+            # Append the row to the rows list
+            rows.append((id, department))
+            # Define the INSERT query to insert rows into the MySQL table
+            insert_query = "INSERT INTO tb_departments (id, department) VALUES (%s, %s)"
+
+        if(file.filename == 'hired_employees.csv'):
+            id = int(values[0])
+            name = values[1]
+            datetime = values[2]
+            department_id = int(values[3])
+            job_id = int(values[4])
+
+            # Append the row to the rows list
+            rows.append((id, name, datetime, department_id, job_id))
+            # Define the INSERT query to insert rows into the MySQL table
+            insert_query = "INSERT INTO tb_jobs (id, job) VALUES (%s, %s)"
+
+        if(file.filename == 'jobs.csv'):
+            id = int(values[0])
+            job = values[1]
+            # Append the row to the rows list
+            rows.append((id, job))
+            # Define the INSERT query to insert rows into the MySQL table
+            insert_query = "INSERT INTO tb_jobs (id, job) VALUES (%s, %s)"
+
         # If the rows list has reached the batch size, insert the rows into the table
         if len(rows) == batch_size:
             cursor.executemany(insert_query, rows)
