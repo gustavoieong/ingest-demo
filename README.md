7e811f74f12a00f0a15d02961a388e7200c25cb9
Gustavo Ieong <gustavo.ieong@gmail.com>
2023-04-28 21:32:36 -0500

    release

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # ingest-demo
 
-#Install dependencies
-sudo apt install python3-pip
-pip install "fastapi[all]"
+# Install dependencies for REST API
+apt install python3-pip
+pip3 install "fastapi[all]"
 
-#Execute main.py for testing
+# Install dependencies for sending files
+pip3 install requests
+
+# Install dependencies for connecting database
+pip3 install mysql-connector-python
+
+# Execute main.py for testing
 python3 -m uvicorn main:app --reload
 
+# Send CSV files
+python3 sender.py
 
+https://docs.amplify.aws/cli/start/install/#configure-the-amplify-cli
 
 Data source is stored at ingestdemo S3 bucket.
--- main.py
+++ main.py
@@ -1,8 +1,107 @@
-from fastapi import FastAPI
+import csv
+import mysql.connector
+from fastapi import FastAPI, File, UploadFile
+import traceback
 
 app = FastAPI()
 
+def generate_table(string filename):
+    create_table_query = ""
+    if (file == 'departments.csv'):
+        create_table_query = """CREATE TABLE IF NOT EXISTS tb_departments (
+                             id INT,
+                             name VARCHAR(255),
+                             datetime VARCHAR(255),
+                             department_id INT,
+                             job_id INT
+                             );"""
+    if (file == 'hired_employees.csv'):
+        create_table_query = """CREATE TABLE IF NOT EXISTS tb_hired_employees (
+                             id INT,
+                             department VARCHAR(255)
+                             );"""
+    if (file == 'jobs.csv'):
+        create_table_query = """CREATE TABLE IF NOT EXISTS tb_jobs (
+                             id INT,
+                             job VARCHAR(255)
+                             );"""
+    return create_table_query
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def generate_query(string filename):
+    insert_query = ""
+    if (file == 'departments.csv'):
+        insert_query = """INSERT INTO my_table (id, name, datetime, department_id, job_id) VALUES (%d, %s, %s, %d, %d);"""
+    if (file == 'departments.csv'):
+        insert_query = """INSERT INTO my_table (id, job) VALUES (%d, %s);"""
+    if (file == 'departments.csv'):
+        insert_query = """INSERT INTO my_table (id, job) VALUES (%d, %s);"""
+    return insert_query
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    # Parse the CSV file
+    contents = await file.read()
+    csv_reader = csv.reader(contents.decode().splitlines())
+    #print(contents.decode())
+    print(file.filename)
+
+    # Connect to the MySQL database
+    cnx = mysql.connector.connect(user='admin', password='Azure123.', host='ingest-mysql.conxcscqngr8.us-east-1.rds.amazonaws.com', database='ingest-mysql')
+    cursor = cnx.cursor()
+
+    # Create table if it doesn't exist
+    create_table_query = generate_table(file.filename)
+    cursor.execute(create_table_query)
+
+    # Set insert query
+    insert_query = generate_query(file.filename)
+
+    # Open the CSV file for reading
+    with open('file.filename') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Insert rows in batches of 1000
+        batch_size = 1000
+        batch = []
+        for row in reader:
+            # Append the row to the batch
+            batch.append(row)
+
+            # If the batch is full, insert the rows and reset the batch
+            if len(batch) == batch_size:
+                insert_query = generate_query(file.filename)
+                try:
+                    cursor.executemany(insert_query)
+                except Exception as e:
+                    # log the error message to a file
+                    damage_row = str(row)
+                    with open('error_log.txt', 'a') as f:
+                        f.write(f"Error message: {str(e)}\n")
+                        f.write(f"Error message: {str(damage_row)}\n")
+                        f.write(traceback.format_exc() + "\n")
+                    # continue execution, skipping the line that raised the exception
+                    pass
+                cnx.commit()
+                batch = []
+
+        # Insert any remaining rows
+        if batch:
+            insert_query = generate_query(file.filename)
+            try:
+                cursor.executemany(insert_query)
+            except Exception as e:
+                # log the error message to a file
+                damage_row = str(row)
+                with open('error_log.txt', 'a') as f:
+                    f.write(f"Error message: {str(e)}\n")
+                    f.write(f"Error message: {str(damage_row)}\n")
+                    f.write(traceback.format_exc() + "\n")
+                # continue execution, skipping the line that raised the exception
+                pass
+            cnx.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    cnx.close()
+
+    return {"filename": file.filename}
